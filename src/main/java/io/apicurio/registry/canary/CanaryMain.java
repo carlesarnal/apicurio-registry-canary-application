cb3f96b723be401f43e512c22a24202f947d3607
Carles Arnal <carlesarnal92@gmail.com>
2023-01-26 23:21:41 +0100

    Add basic auth operations

--- src/main/java/io/apicurio/registry/canary/CanaryMain.java
+++ src/main/java/io/apicurio/registry/canary/CanaryMain.java
@@ -7,6 +7,7 @@ import io.apicurio.registry.canary.util.RegistryCanaryUtil;
 import io.apicurio.registry.rest.client.RegistryClient;
 import io.apicurio.registry.rest.client.RegistryClientFactory;
 import io.apicurio.registry.rest.v2.beans.ArtifactMetaData;
+import io.apicurio.rest.client.auth.BasicAuth;
 import io.apicurio.rest.client.auth.OidcAuth;
 import io.apicurio.rest.client.auth.exception.AuthErrorHandler;
 import io.apicurio.rest.client.spi.ApicurioHttpClient;
@@ -25,45 +26,65 @@ import org.slf4j.LoggerFactory;
  */
 public class CanaryMain {
 
-    private static final RegistryClient client;
-    private static final Logger log = LoggerFactory.getLogger(CanaryMain.class);
+	private static final RegistryClient oAuthclient;
+	private static final RegistryClient basicClient;
 
-    static {
-        client = createProperClient();
-    }
+	private static final Logger log = LoggerFactory.getLogger(CanaryMain.class);
 
-    public static void main(String[] args) throws Exception {
-        try {
-            while (true) {
-                // Register the JSON Schema schema in Apicurio registry.
-                final String artifactId = UUID.randomUUID().toString();
-                RegistryCanaryUtil.createSchemaInServiceRegistry(client, artifactId, Constants.SCHEMA);
+	static {
+		oAuthclient = createOauthClient();
+		basicClient = createBasicClient();
+	}
 
-                //Wait for the artifact to be available.
-                Thread.sleep(1000);
+	public static void main(String[] args) throws Exception {
+		try {
+			while (true) {
+                log.info("Starting ouath-based apicurio registry monitoring");
+                monitorApicurioRegistry(oAuthclient);
+                log.info("Starting basic auth-based apicurio registry monitoring");
+                monitorApicurioRegistry(basicClient);
+			}
+		} catch (Exception e) {
+			log.error("Exception detected in the canary application: {}", e.getCause(), e);
+		}
+	}
 
-                final ArtifactMetaData schemaFromRegistry = RegistryCanaryUtil.getSchemaFromRegistry(client,
-                        artifactId);
-                RegistryCanaryUtil.deleteSchema(client, artifactId);
+	private static void monitorApicurioRegistry(RegistryClient client) throws InterruptedException {
+		// Register the JSON Schema schema in Apicurio registry.
+		final String artifactId = UUID.randomUUID().toString();
+		RegistryCanaryUtil.createSchemaInServiceRegistry(client, artifactId, Constants.SCHEMA);
 
-                //Wait 500ms for the next canary execution.
-                Thread.sleep(500);
-            }
-        } catch (Exception e) {
-            log.error("Exception detected in the canary application: {}", e.getCause(), e);
-        }
-    }
+		//Wait for the artifact to be available.
+		Thread.sleep(1000);
+		final ArtifactMetaData schemaFromRegistry = RegistryCanaryUtil.getSchemaFromRegistry(client,
+				artifactId);
+		RegistryCanaryUtil.deleteSchema(client, artifactId);
 
-    public static RegistryClient createProperClient() {
-        final String registryUrl = System.getenv("REGISTRY_URL");
-        final String tokenEndpoint = System.getenv("AUTH_TOKEN_ENDPOINT");
-        if (tokenEndpoint != null) {
-            final String authClient = System.getenv("AUTH_CLIENT_ID");
-            final String authSecret = System.getenv("AUTH_CLIENT_SECRET");
-            final ApicurioHttpClient httpClient = ApicurioHttpClientFactory.create(tokenEndpoint, Collections.emptyMap(), null, new AuthErrorHandler());
-            return RegistryClientFactory.create(registryUrl, Collections.emptyMap(), new OidcAuth(httpClient, authClient, authSecret));
-        } else {
-            return RegistryClientFactory.create(registryUrl);
-        }
-    }
+		//Wait 500ms for the next canary execution.
+		Thread.sleep(500);
+
+	}
+
+	public static RegistryClient createOauthClient() {
+		final String registryUrl = System.getenv("REGISTRY_URL");
+		final String tokenEndpoint = System.getenv("AUTH_TOKEN_ENDPOINT");
+		if (tokenEndpoint != null) {
+			final String authClient = System.getenv("AUTH_CLIENT_ID");
+			final String authSecret = System.getenv("AUTH_CLIENT_SECRET");
+			final ApicurioHttpClient httpClient = ApicurioHttpClientFactory.create(tokenEndpoint,
+					Collections.emptyMap(), null, new AuthErrorHandler());
+			return RegistryClientFactory.create(registryUrl, Collections.emptyMap(),
+					new OidcAuth(httpClient, authClient, authSecret));
+		} else {
+			return RegistryClientFactory.create(registryUrl);
+		}
+	}
+
+	public static RegistryClient createBasicClient() {
+		final String registryUrl = System.getenv("REGISTRY_URL");
+		final String authClient = System.getenv("AUTH_CLIENT_ID");
+		final String authSecret = System.getenv("AUTH_CLIENT_SECRET");
+		return RegistryClientFactory.create(registryUrl, Collections.emptyMap(),
+				new BasicAuth(authClient, authSecret));
+	}
 }
